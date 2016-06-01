6347df4569252dab7ce292961655351e3e4ea381
anancds <anancds@163.com>
2016-06-01 18:04:27 +0800

    update

--- learn-scala/src/main/scala/com/cds/learnscala/array/ArrayTest.scala
+++ learn-scala/src/main/scala/com/cds/learnscala/array/ArrayTest.scala
@@ -1,6 +1,7 @@
 package com.cds.learnscala.array
 
 import scala.collection.mutable.ArrayBuffer
+import scala.math.random
 
 object ArrayTest {
 
@@ -20,10 +21,37 @@ object ArrayTest {
     println(b)
 
     println(b.sum)
+    println(b.max)
+    println(b.sorted)
+    println(b.sortWith(_ > _))
+    println(b.mkString(" and "))
+    println(b.mkString("<", ",", ">"))
+  }
+
+  //以下是快学scala第三章的课后习题
+  def randomArray(n: Int): Array[Int] = {
+    val a = for (i <- 0 until n) yield (random * n).toInt
+    a.toArray
+  }
+
+  def reorderArray(arr: Array[Int]): Array[Int] = {
+    val t = arr.toBuffer
+    for (i <- 0 until(arr.length, 2) if i + 1 < t.length) {
+      val a = t(i)
+      val b = t(i + 1)
+      t.remove(i, 2)
+      t.insert(i, b)
+      t.insert(i + 1, a)
+
+    }
+    t.toArray
   }
 
   def main(args: Array[String]) {
-    testArrayBuffer()
+    //    testArrayBuffer()
+
+
+//    println(randomArray(10).toBuffer)
   }
 
 }
