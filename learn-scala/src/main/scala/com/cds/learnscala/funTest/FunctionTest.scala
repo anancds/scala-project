ad2885af02ba0afc2d568a8a77c704f8c20d157e
anancds <anancds@163.com>
2016-06-07 15:07:12 +0800

    update

--- learn-scala/src/main/scala/com/cds/learnscala/funTest/FunctionTest.scala
+++ learn-scala/src/main/scala/com/cds/learnscala/funTest/FunctionTest.scala
@@ -11,11 +11,26 @@ object FunctionTest {
     println(fun(num))
 
     println(Array(3.14, 1.42, 2.0).map(fun).toBuffer)
+
+    println((1 to 9).filter(_ % 2 == 0))
+
+    println((1 to 9).reduceLeft(_ * _))
+    println((1 to 9).product)
+
+    println("Mary has a little lamb".split(" ").sortWith(_.length > _.length).toBuffer)
+
   }
+
+  def mulBy(factor: Double) = (x: Double) => factor * x
 
   def main(args: Array[String]) {
 
     test()
+
+    val triple = mulBy(3)
+    val half = mulBy(0.5)
+
+    println(triple(14) + " " + half(14))
 
   }
 
