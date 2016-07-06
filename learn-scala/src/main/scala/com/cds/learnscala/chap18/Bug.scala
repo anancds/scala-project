7e5ba91da6cd640560dbdb6f527d1e48082b4f11
anancds <anancds@163.com>
2016-07-06 11:00:42 +0800

    add chap18

---
+++ learn-scala/src/main/scala/com/cds/learnscala/chap18/Bug.scala
@@ -0,0 +1,30 @@
+package com.cds.learnscala.chap18
+
+class Bug(var pos: Int = 0) {
+
+  var forword: Int = 1
+
+  def move(up: Int): this.type = {
+    pos += forword * up
+    this
+  }
+
+  def show(): this.type = {
+    print(pos + " ")
+    this
+  }
+
+  def turn(): this.type = {
+    forword = -forword
+    this
+  }
+
+
+}
+
+object Test {
+  def main(args: Array[String]) {
+    val bugsy = new Bug()
+    bugsy.move(4).show().move(6).show().turn().move(5).show()
+  }
+}
---
+++ learn-scala/src/main/scala/com/cds/learnscala/chap18/Test1.scala
@@ -0,0 +1,38 @@
+package com.cds.learnscala.chap18
+
+object show
+
+//noinspection ScalaDeprecatedIdentifier
+object then
+
+object around
+
+class Bug1(var pos: Int = 0) {
+  var forword: Int = 1
+
+  def move(num: Int): this.type = {
+    pos += num
+    this
+  }
+
+  //noinspection ScalaDeprecatedIdentifier
+  def and(obj: then.type): this.type = this
+
+  def and(obj: show.type): this.type = {
+    print(pos + " ")
+    this
+  }
+
+  def turn(obj: around.type): this.type = {
+    pos = 0
+    this
+  }
+}
+
+//noinspection ScalaDeprecatedIdentifier
+object Test1 {
+  def main(args: Array[String]) {
+    val bugsy = new Bug1
+    bugsy move 4 and show and then move 6 and show turn around move 4 and show
+  }
+}
