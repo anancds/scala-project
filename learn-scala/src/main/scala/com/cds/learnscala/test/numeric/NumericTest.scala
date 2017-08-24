25e07273d2f9e278079d2432426136f9651e8127
anancds <anancds@163.com>
2017-08-24 15:59:42 +0800

    Added Numeric test

--- learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
+++ learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
@@ -1,10 +1,18 @@
 package com.cds.learnscala.test.numeric
 
+import org.slf4j.{Logger, LoggerFactory}
+
+
 object NumericTest {
+  val Log = LoggerFactory.getLogger(NumericTest.getClass)
   def main(args: Array[String]): Unit = {
     val f = 1.0f
     println(f.toString.toFloat)
 //    println(f.toString.toInt)
+
+    val test = null
+    Log.info(test)
+    println(test)
   }
 
 }
