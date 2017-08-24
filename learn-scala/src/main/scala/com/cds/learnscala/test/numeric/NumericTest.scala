4fa354ace0ef99de6a02c6ad3f25ec33edf5a6f2
anancds <anancds@163.com>
2017-08-24 14:11:39 +0800

    Added Numeric test

---
+++ learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
@@ -0,0 +1,10 @@
+package com.cds.learnscala.test.numeric
+
+object NumericTest {
+  def main(args: Array[String]): Unit = {
+    val f = 1.0f
+    println(f.toString.toFloat)
+//    println(f.toString.toInt)
+  }
+
+}
