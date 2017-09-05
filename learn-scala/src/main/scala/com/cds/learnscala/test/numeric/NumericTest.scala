8bd1add52fa003bd06eab5c74d97e1cea46aaf38
anancds <anancds@163.com>
2017-09-05 15:47:56 +0800

    Added OptionTest

--- learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
+++ learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
@@ -4,11 +4,25 @@ import org.slf4j.{Logger, LoggerFactory}
 
 
 object NumericTest {
+
+  def OptionTest(option: Option[Long]) = {
+    if (option.isEmpty) {
+      None
+    }
+    option match {
+      case None => None
+      case Some(_) => None
+    }
+  }
+
   val Log = LoggerFactory.getLogger(NumericTest.getClass)
+
   def main(args: Array[String]): Unit = {
+
+    OptionTest(None)
     val f = 1.0f
     println(f.toString.toFloat)
-//    println(f.toString.toInt)
+    //    println(f.toString.toInt)
 
     val test = null
     Log.info(test)
@@ -16,7 +30,8 @@ object NumericTest {
 
     val value: Integer = new Integer((100))
     println(value.asInstanceOf[Long])
-    val value1 = value.asInstanceOf[Long]
+//    val value1 = value.asInstanceOf[Long]
+    val value1 = value.asInstanceOf[Int]
     println(value1)
   }
 
