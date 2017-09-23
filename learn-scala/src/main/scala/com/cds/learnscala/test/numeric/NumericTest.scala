9139420f97891d84dc1fec3f3d92a7bec44be236
anancds <anancds@163.com>
2017-09-23 15:27:20 +0800

    Added double和float的区别

--- learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
+++ learn-scala/src/main/scala/com/cds/learnscala/test/numeric/NumericTest.scala
@@ -2,6 +2,8 @@ package com.cds.learnscala.test.numeric
 
 import org.slf4j.{Logger, LoggerFactory}
 
+import scala.collection.mutable.ArrayBuffer
+
 
 object NumericTest {
 
@@ -15,22 +17,32 @@ object NumericTest {
     }
   }
 
+  def testDoubleFload(): Unit = {
+    val row = ArrayBuffer[Any]()
+    row += 252.99
+    row += 252.99f
+    val test1 = row.toArray.asInstanceOf[Array[Object]]
+    println(test1)
+  }
+
   val Log = LoggerFactory.getLogger(NumericTest.getClass)
 
   def main(args: Array[String]): Unit = {
 
     OptionTest(None)
-    val f = 1.0f
+    val f = 252.99
+    println(f.asInstanceOf[Object])
+
     println(f.toString.toFloat)
     //    println(f.toString.toInt)
-
+    testDoubleFload()
     val test = null
     Log.info(test)
     println(test)
 
     val value: Integer = new Integer((100))
     println(value.asInstanceOf[Long])
-//    val value1 = value.asInstanceOf[Long]
+    //    val value1 = value.asInstanceOf[Long]
     val value1 = value.asInstanceOf[Int]
     println(value1)
   }
